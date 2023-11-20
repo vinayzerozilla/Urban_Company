0ee3888103d3c705ac268f46f333923b8751e4a8
“vini1996” <vinaysirige@gmail.com>
2023-11-20 18:38:25 +0530

    service link

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Text, View, TouchableOpacity} from 'react-native';
 
 const Services = props => {
   return (
@@ -7,21 +7,23 @@ const Services = props => {
       style={{
         width: '28%',
       }}>
-      <View
-        style={{
-          backgroundColor: '#D3D3D3',
-          padding: 15,
-          borderRadius: 14,
-          height: 100,
-        }}>
-        <Image
-          style={{width: 75, height: 75}}
-          source={require('./../Assets/images/dummyimage.png')}
-        />
-      </View>
-      <Text style={{textAlign: 'center', color: 'black'}}>
-        {props.servicename}
-      </Text>
+      <TouchableOpacity>
+        <View
+          style={{
+            backgroundColor: '#D3D3D3',
+            padding: 15,
+            borderRadius: 14,
+            height: 100,
+          }}>
+          <Image
+            style={{width: 75, height: 75}}
+            source={require('./../Assets/images/dummyimage.png')}
+          />
+        </View>
+        <Text style={{textAlign: 'center', color: 'black'}}>
+          {props.servicename}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
